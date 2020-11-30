bf8203d6be933537547a6779e0ebce9dbf4ff3cd
MichailJakovlev <doktor2282@gmail.com>
2020-11-30 18:54:12 +0400

    Fourth commit

--- Michail_Jakovlev_JS/script.js
+++ Michail_Jakovlev_JS/script.js
@@ -1,50 +1,37 @@
-function randomText(text) {
+   // Переменные 
+   
+     var random_style = document.getElementById('random_style');
+     var clear = document.getElementById('clear');
+     var display = document.getElementById('display');
+     var text = document.getElementById('text');
+
+   // Массивы
+
+     var styles = ['style_1', 'style_2', 'style_3', 'style_4', 'style_5', 'style_6'];
+     var texts = ['Hello world', 'Java Script', 'Q-Digital','Michail Jakovlev', 'Random text', 'GitHub'];
+
+   // Функции 
+
+     function generationStyle() {
      
-     var target = document.getElementById('text');
-     var texts = [
-         'Hello world',
-         'Web',
-         'Q-Digital',
-         'Cлучайный текст',
-         'Java Script',
-         'Яковлев Михаил',
-         'Вроде работает как надо',
-         'Второй случайный текст'
-     ];
- 
-     var x = (Math.random() * texts.length) | 0;
-     target.innerText = texts[x];
-}
+     var x = (Math.random() * styles.length) | 0;
+     display.className = styles[x];
 
-function randomStyle(wrapper) {
-
-
-var target = document.getElementById('text');
-     var texts = [
-         'Hello world',
-         'Web',
-         'Q-Digital',
-         'Cлучайный текст',
-         'Java Script',
-         'Яковлев Михаил',
-         'Вроде работает как надо',
-         'Второй случайный текст'
-     ];
- 
-     var style = [
-        'style_1',
-        'style_2',
-        'style_3',
-        'style_4',
-        'style_5',
-        'style_6'
-
-     ]; 
-
-     var x = (Math.random() * texts.length) | 0;
-     target.innerText = texts[x];
-
-     var y = (Math.random() * style.length) | 0;
-     document.querySelector(wrapper).className = style[y];
+     var y = (Math.random() * texts.length) | 0;
+     text.innerText = texts[y];
      
-}
+     }
+
+     function clearAll () {
+
+     display.classList.remove('style_1', 'style_2', 'style_3', 'style_4', 'style_5', 'style_6');
+     text.innerHTML = " ";
+       
+     }
+
+   // События
+    
+     random_style.addEventListener('click', generationStyle);
+     clear.addEventListener('click', clearAll);
+
+   
